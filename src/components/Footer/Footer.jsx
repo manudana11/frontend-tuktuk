c37585301d92acac7ad9cc18f336147fc691f308
jaironf <jaironf.94@gmail.com>
2024-06-05 11:49:34 +0200

    Footer creado

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Footer.scss'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+
+
+
+  return (
+    <div className='footer-container'>
+        <nav className='footer-div'>
+            <div className='links-div'>
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Footer
