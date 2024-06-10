5b8c8d57f8a49cbe9e98ac8cb9667d1d56a01cfb
manudana11 <manueldanapallardo@gmail.com>
2024-06-10 10:16:33 +0200

    cambiando la forma de pintar el post details

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { getPostById } from '../../features/posts/postsSlice';
+import { getPostById, likePost, removeLikePost } from '../../features/posts/postsSlice';
 import { useParams } from 'react-router-dom';
+import { CommentOutlined, LikeFilled, LikeOutlined, SendOutlined } from '@ant-design/icons';
 
 const PostDetails = () => {
     const { _id } = useParams();
@@ -26,27 +27,47 @@ const PostDetails = () => {
         return <div>No posts available.</div>;
     }
 
-    if (posts.length==0) {
+    if (posts.length == 0) {
         return <div>No post available</div>
     }
+
+    const hasLiked = posts.likes.includes(userId);
+    const handleLikeClick = () => {
+        if (hasLiked) {
+            dispatch(removeLikePost(posts._id));
+        } else {
+            dispatch(likePost(posts._id));
+        }
+    };
 
     return (
         <div>
             <h1>Post Details</h1>
-                <div key={posts._id}>
-                    <img src={`https://backend-tuktuk.onrender.com/${posts.imgpost.substring(6)}`} alt="Post image" className="post-image" />
-                    <h2>{posts.caption}</h2>
-                    <p><strong>Location:</strong> {posts.location}</p>
-                    <p><strong>Created At:</strong> {new Date(posts.createdAt).toLocaleString()}</p>
-                    <p><strong>Updated At:</strong> {new Date(posts.updatedAt).toLocaleString()}</p>
-                    <p><strong>Likes:</strong> {posts.likes.length}</p>
-                    {posts.commentsIds.map((comment) => (
-                            <div key={comment._id} className="comment">
-                                <strong>{comment.userId.userName}:</strong> {comment.bodyText}
-                            </div>
-                        ))}
-                    {/* <p><strong>Comments:</strong> {posts.commentsIds}</p> */}
+            <div key={posts._id}>
+                <img src={`https://backend-tuktuk.onrender.com/${posts.imgpost.substring(6)}`} alt="Post image" className="post-image" />
+                <div className="post-actions">
+                    <button onClick={handleLikeClick}>
+                        {hasLiked ? <LikeFilled /> : <LikeOutlined />} {posts.likes.length}
+                    </button>
+                    <button onClick={() => handleComment(posts._id)}>
+                        <CommentOutlined /> {posts.commentsIds.length}
+                    </button>
+                    <button onClick={() => handleSend(posts._id)}>
+                        <SendOutlined />
+                    </button>
                 </div>
+                <h2>{posts.caption}</h2>
+                <p><strong>Location:</strong> {posts.location}</p>
+                <p><strong>Created At:</strong> {new Date(posts.createdAt).toLocaleString()}</p>
+                <p><strong>Updated At:</strong> {new Date(posts.updatedAt).toLocaleString()}</p>
+                <p><strong>Likes:</strong> {posts.likes.length}</p>
+                {posts.commentsIds.map((comment) => (
+                    <div key={comment._id} className="comment">
+                        <strong>{comment.userId.userName}:</strong> {comment.bodyText}
+                    </div>
+                ))}
+                {/* <p><strong>Comments:</strong> {posts.commentsIds}</p> */}
+            </div>
         </div>
     )
 }
