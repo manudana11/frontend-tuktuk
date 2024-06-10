b03ed32e509feeb7bb0e990279f3efc593a099e4
manudana11 <manueldanapallardo@gmail.com>
2024-06-10 21:24:38 +0200

    ya podemos añadir comentarios, aunque no se muestran

--- src/features/comments/commentService.js
+++ src/features/comments/commentService.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const API_URL = 'https://backend-tuktuk.onrender.com/comments';
 
-const createComment = async (comments, _id) => {
+const createComment = async (commentData) => {
+    const { postId, bodyText, userId } = commentData;
     const token = localStorage.getItem('token');
-    const res = await axios.post(API_URL + '/id/' + _id, comments, {
+    const res = await axios.post(`${API_URL}/id/${postId}`, {bodyText, userId}, {
         headers: {
             Authorization:token
         }
