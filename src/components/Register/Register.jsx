9356252ea9f0a9d2ca5c0ae02996640bcd0490fd
manudana11 <manueldanapallardo@gmail.com>
2024-06-11 18:46:11 +0200

    ya nos podemos registrar con fotitos

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { register } from '../../features/auth/authSlice';
 import './Register.scss';
 import { Link } from 'react-router-dom';
+import { notification } from 'antd';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -11,44 +12,60 @@ const Register = () => {
         email: '',
         password: '',
         dateOfBirth: '',
-        profilePic: '',
-    })
-    const { name, email, password, userName, dateOfBirth, profilePic } = formData;
+    });
+
     const dispach = useDispatch()
+
     const onChange = (e) => {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value,
         });
-    }
+    };
+
     const onSubmit = (e) => {
         e.preventDefault();
-        dispach(register(formData))
+        const newFormData = new FormData();
+        newFormData.set('name', formData.name);
+        newFormData.set('userName', formData.userName);
+        newFormData.set('email', formData.email);
+        newFormData.set('password', formData.password);
+        newFormData.set('dateOfBirth', formData.dateOfBirth);
+        if (e.target.profilePic.files[0]) newFormData.set('image', e.target.profilePic.files[0]);
+        dispach(register(newFormData))
+        .then((res) => {
+            notification.success({ message: 'Registration successful!' });
+        })
+        .catch((error) => {
+            console.error(error);
+            notification.error({ message: 'Registration failed!' });
+        });
         console.log('formData', formData)
-    }
+    };
+
   return (
     <form className="form" onSubmit={onSubmit}>
     <p className="title">Register</p>
     <p className="message">Signup now and get full access to our app.</p>
     <div className="flex">
         <label>
-            <input required placeholder="Insert your name" type="text" className="input" name="name" id="name" value={name} onChange={onChange} />
+            <input required placeholder="Insert your name" type="text" className="input" name="name" id="name" value={formData.name} onChange={onChange} />
         </label>
         <label>
-            <input required placeholder="Insert your username" type="text" className="input" name="userName" id="userName" value={userName} onChange={onChange} />
+            <input required placeholder="Insert your username" type="text" className="input" name="userName" id="userName" value={formData.userName} onChange={onChange} />
         </label>
     </div>
     <label>
-        <input required placeholder="Insert your email" type="email" className="input" name="email" id="email" value={email} onChange={onChange} />
+        <input required placeholder="Insert your email" type="email" className="input" name="email" id="email" value={formData.email} onChange={onChange} />
     </label>
     <label>
-        <input required type="date" className="input" name="dateOfBirth" id="dateOfBirth" value={dateOfBirth} onChange={onChange} />
+        <input required type="date" className="input" name="dateOfBirth" id="dateOfBirth" value={formData.dateOfBirth} onChange={onChange} />
     </label>
     <label>
-        <input type="file" className="input" name="profilePic" id="profilePic" value={profilePic} onChange={onChange} />
+        <input type="file" className="input" name="profilePic" id="profilePic" />
     </label>
     <label>
-        <input required placeholder="Insert your password" type="password" className="input" name="password" id="password" value={password} onChange={onChange} />
+        <input required placeholder="Insert your password" type="password" className="input" name="password" id="password" value={formData.password} onChange={onChange} />
     </label>
     <button className="submit" type="submit">Submit</button>
     <p className="signin">Already have an account? <a href="/login">Signin</a></p>
