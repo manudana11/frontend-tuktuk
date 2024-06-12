122f912c5d6302bced1e4f01072c11562995b3e5
jaironf <jaironf.94@gmail.com>
2024-06-12 12:18:46 +0200

    Pruebas en profile

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Header from '../Header/Header'
+
 
 const Welcome = () => {
+  const {user} = useSelector((state) => state.auth)
+
   return (
     <div>
-        <img src="src/assets/Logo-tuktuk-sinverano.png" alt="logo-tuktuk" />
+      {user ? (
+        <span>
+          <Header/>
+        </span>       
+      ): (
+        <span> 
+          <img src="src/assets/Logo-tuktuk-sinverano.png" alt="logo-tuktuk" />
+        </span>
+      )}
     </div>
   )
 }
