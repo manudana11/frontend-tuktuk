abdf2c76c441d2f39b81f4a6d242eb746735d3a5
jaironf <jaironf.94@gmail.com>
2024-06-12 20:06:48 +0200

    espacio entre el footer y la última publicación

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Image, Button, Wrap, WrapItem, Center, Spinner, Card, CardBody } from '@chakra-ui/react';
+import { Image, Button, Wrap, WrapItem, Center, Spinner, Card, CardBody, Text, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { follow, getLoggedUser, getUserById, logout, unFollow } from '../../features/auth/authSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -13,6 +13,7 @@ const Profile = () => {
     const user = useSelector((state) => state.auth.user) || null;
     const posts = useSelector((state) => state.auth.posts);
     const [isFollowing, setIsFollowing] = useState(false);
+
 
     useEffect(() => {
         dispatch(getLoggedUser());
@@ -35,7 +36,6 @@ const Profile = () => {
         }
     };
 
-
     if (!user) {
         return <Spinner color="red.500" justify="center" />;
     }
@@ -44,8 +44,11 @@ const Profile = () => {
         <Wrap spacing='20px' align='center' width='100%' justify='center'>
             <WrapItem>
                 <Center>
-                    <Image borderRadius='full' boxSize='150px' src={user.profilePic ? `https://backend-tuktuk.onrender.com/${user.profilePic.substring(6)}` : 'https://imgs.search.brave.com/gV6Xy99WsNTWpgT2KUNxopKhP45u8QMrrL2DGi5HYxg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc'} alt="User profile" className="post-user-image" />
+                    <Image borderRadius='full' boxSize='130px' src={user.profilePic ? `https://backend-tuktuk.onrender.com/${user.profilePic.substring(6)}` : 'https://imgs.search.brave.com/gV6Xy99WsNTWpgT2KUNxopKhP45u8QMrrL2DGi5HYxg/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc'} alt="User profile" className="post-user-image" />
                 </Center>
+            </WrapItem>
+            <WrapItem>  
+                <Text as='b'>{user.userName}</Text>
             </WrapItem>
             <WrapItem>
                 <Center w='150px' h='60px' gap={2}>
